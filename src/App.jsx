ce1a46a1c82bae31e28062f5453e0a390664c6c8
Nova <heetask@gmail.com>
2025-01-03 21:25:48 +0900

    변수 대신 state 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  let post = "강남 우동 맛집";
+  // let post = '강남 우동 맛집';
+  let [글제목1, b] = useState('여자 코트 추천');
+  let [글제목2, c] = useState('강남 우동맛집');
+  let [글제목3, d] = useState('파이썬독학');
 
   return (
     <div>
       <div className="flex bg-black w-full text-white pl-5">
-        <h4 style={{ color: "yellow", fontSize: "20px" }}>블로그</h4>
+        <h4 className="font-bold text-lg py-4">React Blog</h4>
       </div>
-
-      <h4>{post}</h4>
+      <div className="list pl-5 text-left border-b border-b-gray-300">
+        <h4 className="font-bold text-lg py-4">{글제목1}</h4>
+        <p className="py-3">1월 3일 발행</p>
+      </div>
+      <div className="list pl-5 text-left border-b border-b-gray-300">
+        <h4 className="font-bold text-lg py-4">{글제목2}</h4>
+        <p className="py-3">1월 3일 발행</p>
+      </div>
+      <div className="list pl-5 text-left border-b border-b-gray-300">
+        <h4 className="font-bold text-lg py-4">{글제목3}</h4>
+        <p className="py-3">1월 3일 발행</p>
+      </div>
     </div>
   );
 }
