3dd9ca870e49059eb07c87a088775161b834c214
Nova <heetask@gmail.com>
2025-01-03 21:00:13 +0900

    레이아웃 JSX 문법 3개 배우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  let post = "강남 우동 맛집";
 
   return (
-    <>
+    <div>
+      <div className="flex bg-black w-full text-white pl-5">
+        <h4 style={{ color: "yellow", fontSize: "20px" }}>블로그</h4>
+      </div>
 
-    </>
-  )
+      <h4>{post}</h4>
+    </div>
+  );
 }
 
-export default App
+export default App;
