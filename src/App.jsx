e74c2ce242360b579e428649c6a7f804ee05d066
Nova <heetask@gmail.com>
2025-01-03 23:54:29 +0900

    리액트 state 변경하는 법

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   // let post = '강남 우동 맛집';
-  let [글제목1, b] = useState('여자 코트 추천');
-  let [글제목2, c] = useState('강남 우동맛집');
-  let [글제목3, d] = useState('파이썬독학');
+  let [글제목, 글제목변경] = useState([
+    '여자 코트 추천',
+    '강남 우동맛집',
+    '파이썬독학',
+  ]);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div>
@@ -13,15 +16,39 @@ function App() {
         <h4 className="font-bold text-lg py-4">React Blog</h4>
       </div>
       <div className="list pl-5 text-left border-b border-b-gray-300">
-        <h4 className="font-bold text-lg py-4">{글제목1}</h4>
+        <h4 className="font-bold text-lg py-4">
+          {글제목[0]}{' '}
+          <span
+            onClick={
+              // 항상 함수를 넣어야함
+              // 함수명
+              // function() {console.log(1)}
+              // () => { console.log(1);}
+              () => {
+                따봉변경(따봉 + 1);
+              }
+            }
+          >
+            👍
+          </span>{' '}
+          {따봉}
+        </h4>
+        <div
+          onClick={() => {
+            // 숙제: 버튼 클릭시 여자 코트 추천 -> 남자 코트 추천으로 바꾸기
+            글제목변경(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+          }}
+        >
+          글제목 변경
+        </div>
         <p className="py-3">1월 3일 발행</p>
       </div>
       <div className="list pl-5 text-left border-b border-b-gray-300">
-        <h4 className="font-bold text-lg py-4">{글제목2}</h4>
+        <h4 className="font-bold text-lg py-4">{글제목[1]}</h4>
         <p className="py-3">1월 3일 발행</p>
       </div>
       <div className="list pl-5 text-left border-b border-b-gray-300">
-        <h4 className="font-bold text-lg py-4">{글제목3}</h4>
+        <h4 className="font-bold text-lg py-4">{글제목[2]}</h4>
         <p className="py-3">1월 3일 발행</p>
       </div>
     </div>
