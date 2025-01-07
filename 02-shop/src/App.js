6a5fd59119eed44306f86957f07b10385b220dd9
Nova <56466589+nova-fe@users.noreply.github.com>
2025-01-07 05:38:40 +0000

    코드 길어질 경우 import export 사용하기

--- 02-shop/src/App.js
+++ 02-shop/src/App.js
@@ -2,8 +2,12 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 // 1) jsx 안에서 이미지 불러오기: import 작명 from '이미지경로';
 import bg from './img/bg.png';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -26,34 +30,14 @@ function App() {
       {/* 상품목록 */}
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              // public/폴더/이미지 이런식일 경우 아래 방식으로 사용 (권장 방식)
-              src={process.env.PUBLIC_URL + '/logo192.png'}
-              width="80%"
-              alt="상품 이미지1"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-              alt="상품 이미지2"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-              alt="상품 이미지3"
-            />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
+          {/**
+           * 1. 상품목록 컴포넌트화
+           * 2. 데이터바인딩
+           * 3. 반복적인 부분은 map 반복문 써보기
+           */}
+          {shoes.map(function (item, index) {
+            return <Card item={item} index={index} />;
+          })}
         </div>
       </div>
     </div>
@@ -61,3 +45,19 @@ function App() {
 }
 
 export default App;
+
+export function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        // public/폴더/이미지 이런식일 경우 아래 방식으로 사용 (권장 방식)
+        src={`https://codingapple1.github.io/shop/shoes${props.index + 1}.jpg`}
+        width="80%"
+        alt="상품 이미지"
+      />
+      <h4>{props.item.title}</h4>
+      <p>{props.item.content}</p>
+      <p>{props.item.price}</p>
+    </div>
+  );
+}
