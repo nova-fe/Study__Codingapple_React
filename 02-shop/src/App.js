51246b7a51aaef7cebcefd5a31bbd3c9dc38b285
Nova <56466589+nova-fe@users.noreply.github.com>
2025-01-07 01:32:37 +0000

    Bootstrap 설치 및 사용하기

--- 02-shop/src/App.js
+++ 02-shop/src/App.js
@@ -1,8 +1,20 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">My Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
